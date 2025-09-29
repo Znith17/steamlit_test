5d48e9ad839993d9ff8ce60aefc3e8cc67a1defb
Ozkar-Znith <oscarg.pr90@gmail.com>
2025-09-29 01:45:25 -0600

    Update app.py
    Grafico y tabla de resultados

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
+import pandas as pd
 import streamlit as st
 import scipy.stats
 import time
+
+# estas son variables de estado que se conservan cuando Streamlin vuelve a ejecutar este script
+if 'experiment_no' not in st.session_state:
+    st.session_state['experiment_no'] = 0
+
+if 'df_experiment_results' not in st.session_state:
+    st.session_state['df_experiment_results'] = pd.DataFrame(columns=['no', 'iteraciones', 'media'])
 
 st.header('Lanzar una moneda')
 
@@ -30,8 +38,3 @@ start_button = st.button('Ejecutar')
 if start_button:
     st.write(f'Experimento con {number_of_trials} intentos en curso.')
     mean = toss_coin(number_of_trials)
-
-
-#st.write('Esta aplicación aún no es funcional. En construcción.')
-
-
